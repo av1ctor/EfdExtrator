87d2cf150650599e1a44c0c307c4dd28c64e7c74
av1ctor <av1ctortv@gmail.com>
2020-10-09 10:46:51 -0300

    correção na geração

--- scripts/resumos.lua
+++ scripts/resumos.lua
@@ -7,8 +7,6 @@ end
 -- resumo por CFOP na LRE
 function LRE_cfop(db, ws)
 
-	efd_plan_resumos_Reset( ws )
-	
 	ds = db_exec( db, [[
 		select
 			an.cfop, 
@@ -20,7 +18,6 @@ function LRE_cfop(db, ws)
 			iif(sum(an.valorOp) > 0, sum(an.icms) / sum(an.valorOp), 0.0) aliqIcms,
 			sum(an.bcIcmsST) bcIcmsST, 
 			sum(an.icmsST) vlIcmsST,
-			((1.0 - iif(sum(an.valorOp) > 0.0, sum(an.bcIcmsST) / sum(an.valorOp), 1.0))) redBcIcmsST,
 			iif(sum(an.valorOp) > 0.0, sum(an.icmsST) / sum(an.valorOp), 0.0) aliqIcmsST,
 			sum(an.ipi) vlIpi
 			from EFD_Anal an
@@ -50,13 +47,10 @@ end
 -- resumo por CST na LRE
 function LRE_cst(db, ws)
 
-	efd_plan_resumos_Reset( ws )
-	
 	ds = db_exec( db, [[
 		select
 			an.cst, 
-			(select origem from conf.cst c where c.cst = an.cst) origem, 
-			(select tributacao from conf.cst c where c.cst = an.cst) tributacao, 
+			(select origem || ' (' || tributacao || ')' from conf.cst c where c.cst = an.cst) descricao, 
 			sum(an.valorOp) vlOper, 
 			sum(an.bc) bcIcms, 
 			sum(an.icms) vlIcms,
@@ -64,7 +58,6 @@ function LRE_cst(db, ws)
 			iif(sum(an.valorOp) > 0, sum(an.icms) / sum(an.valorOp), 0.0) aliqIcms,
 			sum(an.bcIcmsST) bcIcmsST, 
 			sum(an.icmsST) vlIcmsST,
-			((1.0 - iif(sum(an.valorOp) > 0.0, sum(an.bcIcmsST) / sum(an.valorOp), 1.0))) redBcIcmsST,
 			iif(sum(an.valorOp) > 0.0, sum(an.icmsST) / sum(an.valorOp), 0.0) aliqIcmsST,
 			sum(an.ipi) vlIpi
 			from EFD_Anal an
@@ -91,11 +84,16 @@ function LRE_cst(db, ws)
 end
 
 ----------------------------------------------------------------------
--- criar resumos do Livro de Entradas
-function LRE_criarResumos(db, ws)
+-- criar resumo CFOP do Livro de Entradas
+function LRE_criarResumoCFOP(db, ws)
 
 	xpcall(LRE_cfop, errorHandler, db, ws)
 	
+end
+
+-- criar resumo CST do Livro de Entradas
+function LRE_criarResumoCST(db, ws)
+
 	xpcall(LRE_cst, errorHandler, db, ws)
 	
 end
@@ -104,8 +102,6 @@ end
 -- resumo por CFOP na LRS
 function LRS_cfop(db, ws)
 
-	efd_plan_resumos_Reset( ws )
-	
 	ds = db_exec( db, [[
 		select
 			an.cfop, 
@@ -117,7 +113,6 @@ function LRS_cfop(db, ws)
 			iif(sum(an.valorOp) > 0, sum(an.icms) / sum(an.valorOp), 0.0) aliqIcms,
 			sum(an.bcIcmsST) bcIcmsST, 
 			sum(an.icmsST) vlIcmsST,
-			((1.0 - iif(sum(an.valorOp) > 0.0, sum(an.bcIcmsST) / sum(an.valorOp), 1.0))) redBcIcmsST,
 			iif(sum(an.valorOp) > 0.0, sum(an.icmsST) / sum(an.valorOp), 0.0) aliqIcmsST,
 			sum(an.ipi) vlIpi
 			from EFD_Anal an
@@ -147,13 +142,10 @@ end
 -- resumo por CST na LRS
 function LRS_cst(db, ws)
 
-	efd_plan_resumos_Reset( ws )
-	
 	ds = db_exec( db, [[
 		select
 			an.cst, 
-			(select origem from conf.cst c where c.cst = an.cst) origem, 
-			(select tributacao from conf.cst c where c.cst = an.cst) tributacao, 
+			(select origem || ' (' || tributacao || ')' from conf.cst c where c.cst = an.cst) descricao, 
 			sum(an.valorOp) vlOper, 
 			sum(an.bc) bcIcms, 
 			sum(an.icms) vlIcms,
@@ -161,7 +153,6 @@ function LRS_cst(db, ws)
 			iif(sum(an.valorOp) > 0, sum(an.icms) / sum(an.valorOp), 0.0) aliqIcms,
 			sum(an.bcIcmsST) bcIcmsST, 
 			sum(an.icmsST) vlIcmsST,
-			((1.0 - iif(sum(an.valorOp) > 0.0, sum(an.bcIcmsST) / sum(an.valorOp), 1.0))) redBcIcmsST,
 			iif(sum(an.valorOp) > 0.0, sum(an.icmsST) / sum(an.valorOp), 0.0) aliqIcmsST,
 			sum(an.ipi) vlIpi
 			from EFD_Anal an
@@ -188,10 +179,14 @@ function LRS_cst(db, ws)
 end
 
 ----------------------------------------------------------------------
--- criar resumos do Livro de Saídas
-function LRS_criarResumos(db, ws)
+-- criar resumo CFOP do Livro de Saídas
+function LRS_criarResumoCFOP(db, ws)
 
 	xpcall(LRS_cfop, errorHandler, db, ws)
+end
+
+-- criar resumo CST do Livro de Saídas
+function LRS_criarResumoCST(db, ws)
 
 	xpcall(LRS_cst, errorHandler, db, ws)
 	
